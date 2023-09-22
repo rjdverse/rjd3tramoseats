b9cc23f2cead0e024b3c54cc1e478b1ac3b15a79
AQLT <alain.quartier@yahoo.fr>
2023-09-23 00:57:11 +0200

    refresh correction

--- R/tramoseats.R
+++ R/tramoseats.R
@@ -253,13 +253,15 @@ jtramoseats<-function(ts, spec=c("rsafull", "rsa0", "rsa1", "rsa2", "rsa3", "rsa
 #' @param refspec the reference specification used to define the domain considered for re-estimation ("domain_spec").
 #' By default this is the `"TRFull"` or `"RSAFull"` specification.
 #' @param policy the refresh policy to apply (see details).
-#' @param period,start,end to specify the span on which outliers will be re-identified when `policy` equals to `"Outliers"`
-#' or `"Outliers_StochasticComponent"`. Span definition: \code{period}: numeric, number of observations in a year (12,4...). \code{start}: vector
-#' indicating the start of the series in the format `c(YYYY, MM)`. \code{end}: vector in the format `c(YYYY, MM)` indicating the date from which outliers
+#' @param period,start,end to specify the span on which outliers will not be re-identified when `policy` equals to `"Outliers"`
+#' or `"Outliers_StochasticComponent"`.
+#' Span definition: \code{period}: numeric, number of observations in a year (12, 4...).
+#' \code{start}: vector indicating the start of the series in the format `c(YYYY, MM)`.
+#' \code{end}: vector in the format `c(YYYY, MM)` indicating the date from which outliers
 #' will be re-identified. If span parameters are not specified outliers will be re-detected on the whole series.
 #'
 #' @return a new specification, an object of class `"JD3_X13_SPEC"` (`spec_x13()`),
-#' `"JD3_REGARIMA_SPEC"` (`spec_regarima()`)
+#' `"JD3_REGARIMA_SPEC"` (`spec_regarima()`).
 #'
 #' @references
 #' More information on revision policies in JDemetra+ online documentation:
